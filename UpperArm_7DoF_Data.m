c9e92c1bb1e9d746195cee308e29357f609bae21
Student200514 <200514@umons.ac.be>
2024-05-02 17:38:19 +0200

    Add files via upload

---
+++ UpperArm_7DoF_Data.m
@@ -0,0 +1,20 @@
+%Run this before running the Simscape file
+
+%Human height (m)
+H = 1.8;
+
+%Human mass (kg)
+M = 80;
+
+%Limb lengths (m)
+UA_l = H * 0.186;
+LA_l = H * 0.146;
+HA_l = H * 0.108;
+
+%Limb masses (kg)
+UA_m = M * 0.028;
+LA_m = M * 0.016;
+HA_m = M * 0.006;
+
+%Initial shoulder flexion/extension angle
+IC=0; %in [rad], 0 corresponds to vertical arm pointing towards the ground and pi/2 corresponds to horizontal arm pointing towards the positive x-axis

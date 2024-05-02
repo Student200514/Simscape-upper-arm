2da34dd807f677aeb610083238ffde55ad3efd26
Student200514 <200514@umons.ac.be>
2024-05-02 17:55:54 +0200

    Add files via upload

--- UpperArm_7DoF_Data.m
+++ UpperArm_7DoF_Data.m
@@ -16,5 +16,10 @@ UA_m = M * 0.028;
 LA_m = M * 0.016;
 HA_m = M * 0.006;
 
+%Limb center of mass (m)
+UACOM = 0.436 * UA_l;
+LACOM = 0.430 * LA_l;
+HACOM = 0.506 * HA_l;
+
 %Initial shoulder flexion/extension angle
-IC=0; %in [rad], 0 corresponds to vertical arm pointing towards the ground and pi/2 corresponds to horizontal arm pointing towards the positive x-axis
+IA=-pi/2; %in [rad], -pi/2 corresponds to vertical arm pointing towards the ground and 0 corresponds to horizontal arm pointing towards the positive x-axis
